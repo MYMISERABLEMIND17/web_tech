553e5f0848196f91dcb6d5b3f2137149b4056392
MYMISERABLEMIND17 <tomarparakram17@gmail.com>
2026-04-23 10:25:45 +0530

    updated

--- server/app.js
+++ server/app.js
@@ -11,6 +11,8 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://127.0.0.1:5173',
   'http://127.0.0.1:5174',
+  'http://localhost:5173',
+  'https://web-tech-1-56l0.onrender.com',
 ].filter(Boolean);
 
 app.use(cors({
